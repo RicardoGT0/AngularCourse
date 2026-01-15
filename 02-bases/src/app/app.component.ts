0045072971c66cabd156c50750a601786252ea22
RicardoGT0 <rgonzaleztello@live.com.mx>
2026-01-14 21:07:27 -0600

    Creacion Navbar RouterLink

--- 02-bases/src/app/app.component.ts
+++ 02-bases/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component, inject, isDevMode } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./components/shared/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
