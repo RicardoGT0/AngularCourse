fb8c04a1699c8dc90f3197e08379c6493162165e
RicardoGT0 <rgonzaleztello@live.com.mx>
2026-01-15 21:25:41 -0600

    ajuste a NgClass

--- 02-bases/src/app/pages/dragonball/dragonball-page.component.ts
+++ 02-bases/src/app/pages/dragonball/dragonball-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, signal } from "@angular/core";
+import { NgClass } from "@angular/common";
 
 interface Character {
   id: number;
@@ -9,13 +10,24 @@ interface Character {
 
 @Component({
   templateUrl: "./dragonball-page.component.html",
-  styleUrl: "./dragonball-page.component.css"
+  styleUrl: "./dragonball-page.component.css",
+  imports: [NgClass],
 })
 
 export class DragonballPageComponent {
-  characters=signal<Character[]>([
+  characters = signal<Character[]>([
     { id: 100, name: "Goku", power: 15000 },
-    { id: 200, name: "Vegeta", power: 13000 },
+    { id: 200, name: "Vegeta", power: 12000 },
     { id: 300, name: "Piccolo", power: 7000 },
   ])
+
+  styleByPower(power: number) {
+    if (power > 12000) {
+      return "text-danger";
+    } else if (power > 8000) {
+      return "text-warning";
+    } else {
+      return "text-success";
+    }
+  }
 }
