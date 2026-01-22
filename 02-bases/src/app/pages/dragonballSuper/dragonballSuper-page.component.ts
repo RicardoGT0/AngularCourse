935b2a351eb0bfb37fd0a5e2f9150e433c35c417
RicardoGT0 <rgonzaleztello@live.com.mx>
2026-01-22 17:55:57 -0600

    implementacion de service para la logica

--- 02-bases/src/app/pages/dragonballSuper/dragonballSuper-page.component.ts
+++ 02-bases/src/app/pages/dragonballSuper/dragonballSuper-page.component.ts
@@ -1,7 +1,7 @@
-import { Component, signal } from "@angular/core";
+import { Component, inject, signal } from "@angular/core";
 import { CharacterList } from "../../components/dragonball/character-list/character-list";
-import type { Character } from "../../interfaces/Character.interface";
 import { CharacterAdd } from "../../components/dragonball/character-add/character-add";
+import { DragonballSuperService } from '../../services/dragonballSuperService';
 
 @Component({
   selector: "dragonballSuper",
@@ -11,18 +11,5 @@ import { CharacterAdd } from "../../components/dragonball/character-add/characte
 })
 
 export class DragonballSuperPageComponent {
-  characters = signal<Character[]>([
-    { id: 100, name: "Goku", power: 15000 },
-    { id: 200, name: "Vegeta", power: 12000 },
-    { id: 300, name: "Piccolo", power: 7000 },
-    { id: 400, name: "Krillin", power: 5000 },
-    { id: 500, name: "Gohan", power: 8000 },
-    { id: 600, name: "Trunks", power: 9000 },
-    { id: 700, name: "Frieza", power: 13000 },
-    { id: 800, name: "Yamcha", power: 1000 },
-  ])
-
-  addCharacter(character: Character) {
-    this.characters.update(list => [...list, character]);
-  }
+  DragonballSuperService = inject(DragonballSuperService);
 }
---
+++ 02-bases/src/app/services/dragonballSuperService.ts
@@ -0,0 +1,23 @@
+import { Injectable, signal } from "@angular/core";
+import { Character } from "../interfaces/Character.interface";
+
+
+@Injectable({ providedIn: 'root' })
+export class DragonballSuperService {
+
+  characters = signal<Character[]>([
+    { id: 100, name: "Goku", power: 15000 },
+    { id: 200, name: "Vegeta", power: 12000 },
+    { id: 300, name: "Piccolo", power: 7000 },
+    { id: 400, name: "Krillin", power: 5000 },
+    { id: 500, name: "Gohan", power: 8000 },
+    { id: 600, name: "Trunks", power: 9000 },
+    { id: 700, name: "Frieza", power: 13000 },
+    { id: 800, name: "Yamcha", power: 1000 },
+  ])
+
+  addCharacter(character: Character) {
+    this.characters.update(list => [...list, character]);
+  }
+
+}
