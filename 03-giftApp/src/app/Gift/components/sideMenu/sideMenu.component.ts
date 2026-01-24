abdcddccade3f67a31ad9d704ec2ab9fe05a8db3
RicardoGT0 <rgonzaleztello@live.com.mx>
2026-01-23 19:21:43 -0600

    creacion e implementacion de componente sideMenu

---
+++ 03-giftApp/src/app/Gift/components/sideMenu/sideMenu.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { SideMenuHeaderComponent } from "../sideMenu-header/sideMenu-header.component";
+import { SideMenuOptionsComponent } from "../sideMenu-options/sideMenu-options.component";
+
+@Component({
+  selector: 'side-menu',
+  imports: [SideMenuHeaderComponent, SideMenuOptionsComponent],
+  templateUrl: './sideMenu.component.html',
+})
+export class SideMenuComponent { }
--- 03-giftApp/src/app/Gift/pages/dashboard-page/dashboard-page.component.ts
+++ 03-giftApp/src/app/Gift/pages/dashboard-page/dashboard-page.component.ts
@@ -1,10 +1,9 @@
 import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
-import { SideMenuOptionsComponent } from "../../components/sideMenu-options/sideMenu-options.component";
-import { SideMenuHeaderComponent } from "../../components/sideMenu-header/sideMenu-header.component";
+import { SideMenuComponent } from "../../components/sideMenu/sideMenu.component";
 
 @Component({
   templateUrl: "./dashboard-page.component.html",
-  imports: [RouterOutlet, SideMenuOptionsComponent, SideMenuHeaderComponent],
+  imports: [RouterOutlet, SideMenuComponent],
 })
 export default class DashboardPageComponent {}
