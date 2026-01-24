0fbfc80b9e953ada2cbbaef03b0bd32958105b56
RicardoGT0 <rgonzaleztello@live.com.mx>
2026-01-23 22:37:30 -0600

    correccion Imports

--- 03-giftApp/src/app/Gift/components/sideMenu/sideMenu.component.ts
+++ 03-giftApp/src/app/Gift/components/sideMenu/sideMenu.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { SideMenuHeaderComponent } from "../sideMenu-header/sideMenu-header.component";
-import { SideMenuOptionsComponent } from "../sideMenu-options/sideMenu-options.component";
 
 @Component({
   selector: 'side-menu',
-  imports: [SideMenuHeaderComponent, SideMenuOptionsComponent],
+  imports: [SideMenuHeaderComponent],
   templateUrl: './sideMenu.component.html',
 })
 export class SideMenuComponent { }
