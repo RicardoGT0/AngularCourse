06d7d5a9131102a21f067023a643f08e6363dcbe
RicardoGT0 <rgonzaleztello@live.com.mx>
2026-01-23 19:13:25 -0600

    creacion componente SideMenu-Header

---
+++ 03-giftApp/src/app/Gift/components/sideMenu-header/sideMenu-header.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'side-menu-header',
+  imports: [],
+  templateUrl: './sideMenu-header.component.html',
+})
+export class SideMenuHeaderComponent { }
