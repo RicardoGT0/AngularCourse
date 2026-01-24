80f3812db4783d2e4f21d968fa82d4d53cf5d9f2
RicardoGT0 <rgonzaleztello@live.com.mx>
2026-01-23 19:13:38 -0600

    creacion componente SideMenu-Options

---
+++ 03-giftApp/src/app/Gift/components/sideMenu-options/sideMenu-options.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'side-menu-options',
+  imports: [],
+  templateUrl: './sideMenu-options.component.html',
+})
+export class SideMenuOptionsComponent { }
