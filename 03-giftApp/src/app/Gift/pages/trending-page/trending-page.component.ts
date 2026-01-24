38b17571fea96665b2ffd63d8b76637169a44431
RicardoGT0 <rgonzaleztello@live.com.mx>
2026-01-23 23:29:50 -0600

    implementacion de GifList en Trending-page

--- 03-giftApp/src/app/Gift/pages/trending-page/trending-page.component.ts
+++ 03-giftApp/src/app/Gift/pages/trending-page/trending-page.component.ts
@@ -1,6 +1,8 @@
 import { Component } from "@angular/core";
+import { GifListComponent } from "../../components/gifList/gifList.component";
 
 @Component({
   templateUrl: "./trending-page.component.html",
+  imports: [GifListComponent],
 })
 export default class TrendingPageComponent {}
