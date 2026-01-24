69b8377f6d6e17900ecfbfc345d883f555a6e44f
RicardoGT0 <rgonzaleztello@live.com.mx>
2026-01-23 19:14:18 -0600

    implementacion de rutasHijas y componentes sideMenu

--- 03-giftApp/src/app/Gift/pages/dashboard-page/dashboard-page.component.ts
+++ 03-giftApp/src/app/Gift/pages/dashboard-page/dashboard-page.component.ts
@@ -1,6 +1,10 @@
 import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { SideMenuOptionsComponent } from "../../components/sideMenu-options/sideMenu-options.component";
+import { SideMenuHeaderComponent } from "../../components/sideMenu-header/sideMenu-header.component";
 
 @Component({
   templateUrl: "./dashboard-page.component.html",
+  imports: [RouterOutlet, SideMenuOptionsComponent, SideMenuHeaderComponent],
 })
 export default class DashboardPageComponent {}
