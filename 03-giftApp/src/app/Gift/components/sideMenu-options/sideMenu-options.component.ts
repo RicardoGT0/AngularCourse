34e1fdaa07f0dc1054078ded6572c37998ace7d7
RicardoGT0 <rgonzaleztello@live.com.mx>
2026-01-23 22:39:29 -0600

    Ajuste de logica reutilizable en SideMenu-Options

--- 03-giftApp/src/app/Gift/components/sideMenu-options/sideMenu-options.component.ts
+++ 03-giftApp/src/app/Gift/components/sideMenu-options/sideMenu-options.component.ts
@@ -1,8 +1,25 @@
 import { Component } from '@angular/core';
+import type { MenuOption } from '../../interfaces/MenuOptions.interfaces';
+import { RouterLink, RouterLinkActive } from "@angular/router";
 
 @Component({
   selector: 'side-menu-options',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './sideMenu-options.component.html',
 })
-export class SideMenuOptionsComponent { }
+export class SideMenuOptionsComponent {
+  menuOptions: MenuOption[] = [
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+      subLabel: 'Los gifs mas populares',
+      route: '/dashboard/trending',
+    },
+    {
+      icon: 'fa-solid fa-magnifying-glass',
+      label: 'Search',
+      subLabel: 'Buscar gifs',
+      route: '/dashboard/search',
+    },
+  ];
+}
