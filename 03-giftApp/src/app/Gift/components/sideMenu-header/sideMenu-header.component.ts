183325a7a1004e22fa23340fd46c850b670300ea
RicardoGT0 <rgonzaleztello@live.com.mx>
2026-01-23 22:42:07 -0600

    Implementacion Variables de entorno en SideMenu-Header

--- 03-giftApp/src/app/Gift/components/sideMenu-header/sideMenu-header.component.ts
+++ 03-giftApp/src/app/Gift/components/sideMenu-header/sideMenu-header.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { environment } from '@environments/environment';
 
 @Component({
   selector: 'side-menu-header',
   imports: [],
   templateUrl: './sideMenu-header.component.html',
 })
-export class SideMenuHeaderComponent { }
+export class SideMenuHeaderComponent {
+  envs = environment
+}
