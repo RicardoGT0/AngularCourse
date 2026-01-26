75cb0303e64f5ac50c9ecb5db794def5e665ee9f
RicardoGT0 <rgonzaleztello@live.com.mx>
2026-01-26 14:25:10 -0600

    Cambio de estilo a Masonry

--- 03-giftApp/src/app/Gift/pages/trending-page/trending-page.component.ts
+++ 03-giftApp/src/app/Gift/pages/trending-page/trending-page.component.ts
@@ -7,18 +7,18 @@ import { GifListComponent } from "../../components/gifList/gifList.component";
 })
 export default class TrendingPageComponent {
   trendingGifs: string[] = [
-  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg",
-  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg",
-  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg",
-  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg",
-  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg",
-  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg",
-  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-6.jpg",
-  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-7.jpg",
-  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-8.jpg",
-  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-9.jpg",
-  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-10.jpg",
-  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-11.jpg"
+  "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg",
+  "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg",
+  "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-2.jpg",
+  "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg",
+  "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-4.jpg",
+  "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-5.jpg",
+  "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-6.jpg",
+  "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-7.jpg",
+  "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-8.jpg",
+  "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-9.jpg",
+  "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-10.jpg",
+  "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-11.jpg"
 ];
 
 }
