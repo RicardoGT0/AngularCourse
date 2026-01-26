7f88fc7ab2f39225e14354dcf4ddd4d38b0deaab
RicardoGT0 <rgonzaleztello@live.com.mx>
2026-01-26 08:38:17 -0600

    implementacion de array de imagenes en trending.page

--- 03-giftApp/src/app/Gift/components/gifList/gifList-item/gifList-item.component.ts
+++ 03-giftApp/src/app/Gift/components/gifList/gifList-item/gifList-item.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'gif-list-item',
   imports: [],
   templateUrl: './gifList-item.component.html',
 })
-export class GitListItemComponent { }
+export class GitListItemComponent {
+  gif = input.required<string>()
+ }
--- 03-giftApp/src/app/Gift/components/gifList/gifList.component.ts
+++ 03-giftApp/src/app/Gift/components/gifList/gifList.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { GitListItemComponent } from "./gifList-item/gifList-item.component";
 
 @Component({
@@ -6,4 +6,6 @@ import { GitListItemComponent } from "./gifList-item/gifList-item.component";
   imports: [GitListItemComponent],
   templateUrl: './gifList.component.html',
 })
-export class GifListComponent { }
+export class GifListComponent {
+  gifs = input.required<string[]>()
+}
